4960a11df03d9aea6563eec6442a0281712f545f
Borodatyi <zxcmnj6578@gmail.com>
2018-11-08 15:56:38 +0500

    added test2

--- src/tm/test/Main.java
+++ src/tm/test/Main.java
@@ -3,7 +3,7 @@ package tm.test;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test2");
+        System.out.println("test3");
 	// write your code here
     }
 }
